8e3280a1d27098142228d9a58db5730da389603e
SuperSonic <supersonic@livemail.tw>
2019-12-03 00:40:19 +0800

    Update

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from setuptools import setup
 
+# blink2png  Copyright (C) 2019 Star Inc.
+# This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; type 'show c' for details.
+
 version = '0.9'
 
 setup(
