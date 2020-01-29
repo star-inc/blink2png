f3dd0760e4cfa6e56ab1f9c961668e9b5cd030c4
SuperSonic <supersonic@livemail.tw>
2020-01-29 14:02:30 +0800

    Add README for pypi

--- setup.py
+++ setup.py
@@ -2,19 +2,36 @@
 
 from setuptools import setup
 
-# blink2png  Copyright (C) 2019 Star Inc.
-# This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
-# This is free software, and you are welcome to redistribute it
-# under certain conditions; type 'show c' for details.
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-version = '0.9'
+# blink2png  Copyright (C) 2019 Star Inc.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+version = '0.9b'
 
 setup(
     name="blink2png",
     version=version,
     url='http://github.com/star-inc/blink2png',
     license='GNU General Public License',
-    description="To take snapshot of web pages using Blink Engine and Qt5",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='To take snapshot of web pages using Blink Engine and Qt5',
     author='Star Inc.',
     author_email='"Star Inc." <star-inc@aol.com>',
     packages=['blink2png'],
